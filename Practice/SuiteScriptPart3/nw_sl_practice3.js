ddfff504004455e33bb05c0f072a625cdef58dc9
Anupa Dayaratne <anupaamal@sankaraa.com>
2019-01-29 11:56:34 +0530

    Create file using SuiteScript

--- Practice/SuiteScriptPart3/nw_sl_practice3.js
+++ Practice/SuiteScriptPart3/nw_sl_practice3.js
@@ -2,10 +2,28 @@
  *@NApiVersion 2.0
  *@NScriptType Suitelet
  */
-define([], function() {
+define(['N/file'], function(file) {
 
     function onRequest(context) {
+
+        var LOG_TITLE = "Practice 3_";
+
+        var content = "Testing content goes in here";
         
+        var fileObj = file.create({
+            name: 'outputFile.txt',
+            fileType: file.Type.PLAINTEXT,
+            contents: content,
+            description: 'file description',
+            encoding: file.Encoding.UTF8,
+            folder: 11042,
+            isOnline: true
+        });
+        
+        var idCsv = fileObj.save();
+
+        log.debug(LOG_TITLE, idCsv);
+
     }
 
     return {
